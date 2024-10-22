e7e610e8133bcd5844d92764361c9dcbdce521c1
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2024-10-22 12:13:31 +0600

    remove bookmark added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import { FaBookmark } from 'react-icons/fa';
 
 const Blog = ({blog, handleAddToBookmark, handleMarkAsRead}) => {
-    const {title, cover, reading_time, author, author_img, posted_date, hashtags} = blog;
+    const {id, title, cover, reading_time, author, author_img, posted_date, hashtags} = blog;
     return (
         <div className='mb-20 space-y-4'>
             <img className='w-full mb-8' src={cover} alt={`cover picture of the title ${title}`} />
@@ -27,7 +27,7 @@ const Blog = ({blog, handleAddToBookmark, handleMarkAsRead}) => {
                     hashtags.map((hash, idx) => <span key={idx}><a href="">#{hash}</a></span>)
                 }
             </p>
-            <button onClick={()=> handleMarkAsRead(reading_time)} className='text-purple-600 font-bold underline'>Mark As Read</button>
+            <button onClick={()=> handleMarkAsRead(id, reading_time)} className='text-purple-600 font-bold underline'>Mark As Read</button>
         </div>
     );
 };
