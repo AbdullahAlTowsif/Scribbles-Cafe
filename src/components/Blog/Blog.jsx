8297f5acdc84f95bb52cd0c9018091375042340a
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2024-10-22 10:05:11 +0600

    blog display done

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,32 @@
 import PropTypes from 'prop-types';
+import { FaBookmark } from 'react-icons/fa';
 
-const Blog = ({blog}) => {
-    console.log(blog)
+const Blog = ({blog, handleAddToBookmark}) => {
+    const {title, cover, reading_time, author, author_img, posted_date, hashtags} = blog;
     return (
-        <div>
-            <h1 className="text-4xl">Blogs</h1>
+        <div className='mb-20'>
+            <img className='w-full mb-8' src={cover} alt={`cover picture of the title ${title}`} />
+            <div className='flex justify-between items-center mb-4'>
+                <div className='flex'>
+                    <img className='w-14' src={author_img} alt="" />
+                    <div className='ml-6'>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                    <button 
+                    onClick={handleAddToBookmark}
+                    className='ml-2 text-2xl text-red-600'><FaBookmark></FaBookmark></button>
+                </div>
+            </div>
+            <h2 className='text-4xl'>{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href="">#{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
